c4b3b2fc6e96c7305a94bb368ea1f8963dd84a92
ThanhCongTM <sibibi9@gmail.com>
2017-12-27 10:59:10 +0700

    Test SignIn & SignUp

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { compare, hash } from 'bcrypt';
 import { model, Schema} from 'mongoose';
 
 const userSchema = new Schema({
@@ -8,4 +9,29 @@ const userSchema = new Schema({
 
 const UserMongo = model('User', userSchema);
 
-export class User extends UserMongo {}
+export class User extends UserMongo {
+    password: string;
+    email: string;
+    _id: string;
+    name: string;
+
+    static async signIn(email: string, password: string) {
+//Lay Password
+        const user = await User.findOne({ email}) as User;
+//Neu User khong ton tai Throw ra Loi
+        if (!user) throw new Error('User khong ton tai.');
+//Neu ton tai thi COmpare ra password
+        const same = await compare(password, user.password);
+//Password k ton tai
+        if (!same) throw new Error('Sai password.');
+        return user;
+
+    }
+
+    static async signUp(email: string, password: string, name: string) {
+//TaoUsermoi
+        const encrypted = await hash(password, 8);
+        const user  = new User({ email, name, password: encrypted });
+        return user.save();
+    }
+}
--- test/models/User.test.ts
+++ test/models/User.test.ts
@@ -1,11 +1,30 @@
 import * as assert from 'assert';
 import { User} from '../../src/models/User';
 
-describe('Test user', () => {
+xdescribe('Test user', () => {
     it ('Can create new user', async () => {
         const user = new User({ email: 'si@gmail.com', password: '123', name: 'Mr Si'});
         await user.save();
         const numOfUsers = await User.count({});
         assert.equal(numOfUsers, 1);
+    });
+});
+
+describe('Test Signup', () => {
+    it('Can sign up with full info', async () => {
+        await User.signUp('sangsi@gmail.com', '123', 'Mr Si');
+        const user = await User.findOne({email: 'sangsi@gmail.com' }) as User;
+        assert.equal(user.name, 'Mr Si');
+    });
+
+    it('Cannot sign up with dup email', async () => {
+        await User.signUp('vanpho1@gmail.com', '123', 'Pho');
+        try {
+            await User.signUp('vanpho1@gmail.com', '1234', 'Pho 2');
+        } catch (err) {
+            const count = await User.count({});
+            assert.equal(count, 1);
+            assert.equal(err.code, 11000);
+        }
     });
 });
